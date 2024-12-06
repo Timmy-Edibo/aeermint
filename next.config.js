3aff59c03d033f9029559fbb4bafedcd2a67cc49
joeveloper <bebosir@gmail.com>
2024-12-06 13:00:31 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/', // Match the root path
+          destination: '/wallets', // Redirect to your entry route
+          permanent: true, // Indicates a 308 permanent redirect
+        },
+      ];
+    },
+    reactStrictMode: true, // Optional: Enables React's Strict Mode for better development experience
+    swcMinify: true, // Optional: Enables SWC-based minification for faster builds
+  };
+  
+  module.exports = nextConfig;
+  
