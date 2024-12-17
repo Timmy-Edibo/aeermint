d400f22e840a3f503969183ed0121279eb0dd41d
joeveloper <bebosir@gmail.com>
2024-12-17 15:24:08 +0100

    fix transaction data point

--- app/transaction-details/[uuid]/page.js
+++ app/transaction-details/[uuid]/page.js
@@ -12,8 +12,8 @@ export default function TransactionDetails() {
   const { uuid } = useParams();
   const [parsedData, setParsedData] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  
+  const [userType, setUserType] = useState(null);
+  const [error, setError] = useState(null);
 
   const fetchTransactionDetails = async (uuid) => {
     const storedUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -47,6 +47,10 @@ export default function TransactionDetails() {
   };
 
   useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("currentUser"));
+    storedUser?.account?.interactableType === "USER"
+      ? setUserType("user")
+      : setUserType("vendor");
     if (uuid) {
       fetchTransactionDetails(uuid);
     }
@@ -72,36 +76,43 @@ export default function TransactionDetails() {
                           <span>{parsedData?.transactionRef}</span>
                         </h5>
                       </li>
-                      <li>
-                        <p className="mb-0">
-                          {parsedData?.vendor?.interactableType === "USER" ? "Recipient" : "Sender"}
-                        </p>
-                        <h5 className="mb-0">
-                          {parsedData?.vendor?.interactableType === "USER"? (
+                      {userType === "user" ? (
+                        <li>
+                          <p className="mb-0">Recipient</p>
+                          <h5 className="mb-0">
                             <span>
                               {parsedData?.vendor?.businessName}{" "}
                               {parsedData?.vendor?.businessAddress}
                             </span>
-                          ) : (
+                          </h5>
+                        </li>
+                      ) : (
+                        <li>
+                          <p className="mb-0">Sender</p>
+                          <h5 className="mb-0">
                             <span>
                               {parsedData?.payer?.firstName}{" "}
                               {parsedData?.payer?.lastName}
                             </span>
-                          )}
-                        </h5>
-                      </li>
-                      <li>
-                        <p className="mb-0">
-                          {type === "USER" ? "Recipient Ac/No" : "Sender Ac/No"}
-                        </p>
-                        <h5 className="mb-0">
-                          {parsedData?.vendor?.interactableType === "USER" ? (
+                          </h5>
+                        </li>
+                      )}
+                      {userType === "user" ? (
+                        <li>
+                          <p className="mb-0">Recipient A/c No</p>
+                          <h5 className="mb-0">
                             <span>{parsedData?.vendor?.routableNumber}</span>
-                          ) : (
+                          </h5>
+                        </li>
+                      ) : (
+                        <li>
+                          <p className="mb-0">Sender A/c No</p>
+                          <h5 className="mb-0">
                             <span>{parsedData?.payer?.routableNumber}</span>
-                          )}
-                        </h5>
-                      </li>
+                          </h5>
+                        </li>
+                      )}
+
                       <li>
                         <p className="mb-0">Transaction Amount</p>
                         <h5 className="mb-0">
@@ -121,18 +132,6 @@ export default function TransactionDetails() {
                           <span>{parsedData?.exchangeRate}</span>
                         </h5>
                       </li>
-                      {/* <li>
-                        <p className="mb-0">Vendor Name</p>
-                        <h5 className="mb-0">
-                          <span>{parsedData?.vendorName}</span>
-                        </h5>
-                      </li> */}
-                      {/* <li>
-                        <p className="mb-0">Customer Name</p>
-                        <h5 className="mb-0">
-                          <span>{parsedData?.customerName}</span>
-                        </h5>
-                      </li> */}
                       <li>
                         <p className="mb-0">Transaction Date</p>
                         <h5 className="mb-0">
@@ -145,12 +144,6 @@ export default function TransactionDetails() {
                           <span>{formatTime(parsedData?.createdAt)}</span>
                         </h5>
                       </li>
-                      {/* <li>
-                        <p className="mb-0">Transaction Completion Date</p>
-                        <h5 className="mb-0">
-                          <span>{parsedData?.completedAt}</span>
-                        </h5>
-                      </li> */}
                     </ul>
                   </div>
                 </div>
@@ -159,148 +152,14 @@ export default function TransactionDetails() {
           </div>
         </div>
       </div>
+      {error && (
+        <ToastDisplay
+          message={error}
+          type="error"
+          showToast={true}
+          setShowToast={setError}
+        />
+      )}
     </Layout>
   );
 }
-
-// "use client";
-
-// import ChartjsDonut from "@/components/chart/ChartjsDonut";
-// import Layout from "@/components/layout/Layout";
-// import AnalyticsMenu from "@/components/layout/AnalyticsMenu";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import Loading from "../loading";
-// import ToastDisplay from "../../components/elements/ToastDisplay";
-// import { useEffect, useState } from "react";
-// import { formatDate, formatDateAndTime } from "../../utils/dateAndTimeFormatter";
-// import { baseUrl } from "../../utils/constants";
-
-// export default function TransactionDetails() {
-//   const param = useParams();
-//   const { uuid } = param;
-//   const [parsedData, setParsedData] = useState(null);
-//   const [loading, setLoading] = useState(false);
-
-//   const fetchTransactionDetails = async (uuid) => {
-//     try {
-//       setLoading(true);
-//       const response = await fetch(`${baseUrl}/transactions/user/${uuid}`, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//         },
-//       });
-
-//       if (!response.ok) {
-//         const errorData = await response.json();
-//         throw new Error(errorData.message || "Failed to fetch transaction details");
-//       }
-
-//       const data = await response.json();
-//       console.log("Fetched Data:", data);
-//       setParsedData(data?.data);
-//     } catch (error) {
-//       console.error("Error fetching transaction details:", error.message);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const initializeData = async () => {
-//       if (!uuid) return; // Skip if `uuid` is undefined or null
-
-//       try {
-//         console.log(uuid);
-//         setLoading(true);
-//         await fetchTransactionDetails(uuid);
-//       } catch (error) {
-
-//         console.error("Error initializing transaction details:", error.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     initializeData();
-//   }, []);
-
-//   return (
-//     <>
-//       <Layout breadcrumbTitle="Transaction Details">
-//         {loading && <Loading />}
-//         <div className="row">
-//           <div className="col-xxl-12 col-xl-12">
-//             {/* <AnalyticsMenu /> */}
-//             <div className="d-flex justify-content-around">
-//               <div className="w-50">
-//                 <div className="card">
-//                   <div className="card-header">
-//                     <h4 className="card-title">Transaction Details</h4>
-//                   </div>
-//                   <div className="card-body">
-//                     {/* <ChartjsDonut /> */}
-//                     <div className="list-1 mt-3">
-//                       <ul>
-//                         <li>
-//                           <p className="mb-0">Transaction Ref</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.transactionRef}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Transaction Type</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.transactionType}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Transaction Amount</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.currency}</span><span>{parsedData?.amount}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Transaction Fees</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.fees}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Vendor Name</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.vendorName}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Customer Name</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.customerName}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Transaction Creation Date</p>
-//                           <h5 className="mb-0">
-//                             {/* <span>{formatDateAndTime(parsedData?.createdAt)}</span> */}
-//                             <span>{formatDateAndTime(parsedData?.createdAt)}</span>
-//                           </h5>
-//                         </li>
-//                         <li>
-//                           <p className="mb-0">Transaction Completion Date</p>
-//                           <h5 className="mb-0">
-//                             <span>{parsedData?.completionDate}</span>
-//                           </h5>
-//                         </li>
-//                       </ul>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </Layout>
-//     </>
-//   );
-// }
